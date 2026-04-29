266bbbf00f2f94693375b540b970fdd3563ea925
Philida <philbabe@rocketmail.com>
2026-04-29 21:30:41 +0200

    fix mongoose connection (remove deprecated options)

--- server.js
+++ server.js
@@ -11,12 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 // ================= MONGODB =================
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected ✅"))
-.catch((err) => console.log("MongoDB error ❌", err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected ✅"))
+  .catch((err) => console.log("MongoDB error ❌", err));
 
 // ================= MODEL =================
 const userSchema = new mongoose.Schema({
