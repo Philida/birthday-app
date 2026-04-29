c3d1e52359de80f7c1df3225daebbc8fede6c5a1
Philida <philbabe@rocketmail.com>
2026-04-29 20:35:16 +0200

    fix express wildcard route

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ cron.schedule("* * * * *", async () => {
 // ================= SERVE FRONTEND =================
 app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
 });
 
